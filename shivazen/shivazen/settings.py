d7b18b4ca206209f6544c5ecd737c4d9bc641193
StefanyCarso <stefanycarso@yahoo.com.br>
2025-05-21 00:02:01 -0300

    Ajuste de codigo

--- shivazen/shivazen/settings.py
+++ shivazen/shivazen/settings.py
@@ -122,6 +122,8 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # Desenvolvimento
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Produção
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'  # Para uploads de usuários
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
